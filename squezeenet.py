38be83ef32e59c9cf83fbb8aef7f3a80c8dbd82c
Alex <alexburlacu96@gmail.com>
2016-10-06 13:07:41 +0300

    small fix

--- squezeenet.py
+++ squezeenet.py
@@ -12,17 +12,21 @@ tn.config.openmp = True
 OMP_NUM_THREADS = mp.cpu_count()
 
 class FireModule:
-  def __init__(self, squeeze_size, expand_size):
+  def __init__(self, squeeze_size, expand_size, stride = 1):
     self.sqz_size = squeeze_size
     self.expn_size = expand_size
+    self.stride = (stride, stride)
 
   def __call__(self, data):
     # squeeze layer
-    sqz_layer = Convolution2D(self.sqz_size, 1, 1, activation = "relu")(data)
+    sqz_layer = Convolution2D(self.sqz_size, 1, 1,
+                            subsample = self.stride, activation = "relu")(data)
     
     # expand layer
-    conv_1x1 = Convolution2D(self.expn_size, 1, 1, activation = "relu")(sqz_layer)
-    conv_3x3 = Convolution2D(self.expn_size, 3, 3, activation = "relu")(sqz_layer)
+    conv_1x1 = Convolution2D(self.expn_size, 1, 1,
+                            subsample = self.stride, activation = "relu")(sqz_layer)
+    conv_3x3 = Convolution2D(self.expn_size, 3, 3,
+                            subsample = self.stride, activation = "relu")(sqz_layer)
 
-    return megre([conv_1x1, conv_3x3], mode = "concat", concat_axis = 1)
+    return merge([conv_1x1, conv_3x3], mode = "concat", concat_axis = 1)
 
