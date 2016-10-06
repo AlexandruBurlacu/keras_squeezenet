88ffa3b7530b502d355f987ffa3405618d13e821
Alex <alexburlacu96@gmail.com>
2016-10-06 13:49:39 +0300

    debug firemodule.py

--- firemodule.py
+++ firemodule.py
@@ -19,14 +19,15 @@ class FireModule:
 
   def __call__(self, data):
     # squeeze layer
-    sqz_layer = Convolution2D(self.sqz_size, 1, 1,
+    sqz_layer = Convolution2D(self.sqz_size, 1, 1, border_mode = "same",
                             subsample = self.stride, activation = "relu")(data)
     
     # expand layer
-    conv_1x1 = Convolution2D(self.expn_size, 1, 1,
+    conv_1x1 = Convolution2D(self.expn_size, 1, 1, border_mode = "same",
                             subsample = self.stride, activation = "relu")(sqz_layer)
-    conv_3x3 = Convolution2D(self.expn_size, 3, 3,
+    conv_3x3 = Convolution2D(self.expn_size, 3, 3, border_mode = "same",
                             subsample = self.stride, activation = "relu")(sqz_layer)
 
     return merge([conv_1x1, conv_3x3], mode = "concat", concat_axis = 1)
+
 
